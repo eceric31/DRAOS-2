2ea248b1520d95dac69419172c782ee2e174cd8c
adnanba <ababovic1@etf.unsa.ba>
2018-02-19 08:57:23 +0100

    removed cart migrations

--- db/migrate/20180209104756_remove_quantity_from_cart.rb
+++
@@ -1,5 +0,0 @@
-class RemoveQuantityFromCart < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :carts, :quantity, :integer
-  end
-end
--- db/migrate/20180209105242_add_cart_to_cart_shoe_detail.rb
+++
@@ -1,5 +0,0 @@
-class AddCartToCartShoeDetail < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :cart_shoe_details, :cart, foreign_key: true
-  end
-end
