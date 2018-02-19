418173417d7da20b89339eee5039344ce8be29f9
adnanba <ababovic1@etf.unsa.ba>
2018-02-19 08:59:11 +0100

    removed migration

--- db/migrate/20180213091553_drop_tables.rb
+++
@@ -1,8 +0,0 @@
-class DropTables < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :carts, force: :cascade
-    drop_table :cart_shoe_details, force: :cascade
-    drop_table :purchases, force: :cascade
-    drop_table :purchase_details, force: :cascade
-  end
-end
