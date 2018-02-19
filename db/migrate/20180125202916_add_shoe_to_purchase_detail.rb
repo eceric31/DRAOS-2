af1ac3bf32071babc401b2e9de7ec20945062717
adnanba <ababovic1@etf.unsa.ba>
2018-02-19 08:50:44 +0100

    removed migrations

--- db/migrate/20180125202916_add_shoe_to_purchase_detail.rb
+++
@@ -1,5 +0,0 @@
-class AddShoeToPurchaseDetail < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :purchase_details, :shoe, foreign_key: true
-  end
-end
--- db/migrate/20180125202926_add_purchase_to_purchase_detail.rb
+++
@@ -1,5 +0,0 @@
-class AddPurchaseToPurchaseDetail < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :purchase_details, :purchase, foreign_key: true
-  end
-end
