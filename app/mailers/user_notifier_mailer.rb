6194a769ccea5ba442287f321c5af485d97ffc6e
Edin Cerić <>
2019-02-16 12:36:59 +0100

    Initial commit

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,16 +1,16 @@
 class UserNotifierMailer < ApplicationMailer
-    default :from => 'abhshoes@abh.com'
+    default :from => 'eds@eds.com'
 
     def send_signup_email(user)
       @user = user
       mail( to: @user.email,
-      subject: 'Thanks for signing up for abhShoes!' )
+      subject: 'Thanks for signing up for Eds Kingdom!' )
     end
 
     def send_purchase_confirmation_email(user, order)
         @user = user
         @order = order
         mail( to: @user.email,
-        subject: 'Thank you for your purchase at abhShoes' )
+        subject: 'Thank you for your purchase at Eds Kingdom' )
       end
 end
